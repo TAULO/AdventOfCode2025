82cce85b28b102ed30bcf8e29a2e2eeef3dd2cd0
Thomas Taulo <taulo@live.dk>
2025-11-24 16:03:38 +0100

    add guard

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const path = import.meta.dirname;
 
 for (let i = 1; i < 25; i++) {
     const dayPath = path + '/day ' + i;
+
+    if (fs.existsSync(dayPath)) break;
+
     fs.mkdirSync(dayPath)
 
     fs.mkdirSync(dayPath + '/problem 1')
